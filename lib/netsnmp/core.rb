37d0e0e31c64a9a1d8018709b5759f569f3e5c25
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-05-11 12:32:30 +0200

    initializing net-snmp straight away

--- lib/netsnmp/core.rb
+++ lib/netsnmp/core.rb
@@ -1,13 +1,12 @@
 require 'ffi'
 
-module NETSNMP
-  module Core
-  end
-end
-
 require 'netsnmp/core/libc'
 require 'netsnmp/core/constants'
 require 'netsnmp/core/structures'
 require 'netsnmp/core/libsnmp'
 require 'netsnmp/core/inline'
 require 'netsnmp/core/utilities'
+
+module NETSNMP::Core
+  LibSNMP.init_snmp("snmp")
+end
