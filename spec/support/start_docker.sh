d1ca691b7b2364a39e12f50b4a63672db330f1a3
HoneyryderChuck <cardoso_tiago@hotmail.com>
2016-05-18 10:48:48 +0200

    setting the proxies from the env

--- spec/support/start_docker.sh
+++ spec/support/start_docker.sh
@@ -1,4 +1,4 @@
-sudo docker build -t snmp-server-emulator -f spec/support/Dockerfile .
+sudo docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t snmp-server-emulator -f spec/support/Dockerfile .
 sudo docker run -d -p :1161/udp --name test-snmp-emulator snmp-server-emulator --agent-udpv4-endpoint=0.0.0.0:1161 --agent-udpv6-endpoint='[::0]:1161'
 
 
