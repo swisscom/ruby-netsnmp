8544c998cd973b6236958cfa863db2a0ea363140
HoneyryderChuck <cardoso_tiago@hotmail.com>
2016-05-19 10:59:50 +0200

    do not spec stuff which is an OS variable

--- spec/core/libc_spec.rb
+++ spec/core/libc_spec.rb
@@ -2,16 +2,4 @@ RSpec.describe NETSNMP::Core::C do
   it "exposes the free function" do
     expect(described_class).to respond_to(:free)
   end 
-  it "wrapes the fdset struc as FDSet" do
-    fdset = NETSNMP::Core::C::FDSet.new
-    if FFI::Platform::IS_WINDOWS
-      expect(fdset[:fd_count]).to be_zero
-      expect(fdset[:fd_array]).not_to be_nil
-      expect(fdset[:fd_array].size).to be(2048)
-    else
-      expect(fdset[:fds_bits]).not_to be_nil
-      expect(fdset[:fds_bits].size).to be(128)
-
-    end
-  end
 end
