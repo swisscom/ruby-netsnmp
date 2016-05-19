27972209e5bef3c918dc805aa5029f8484fb6bec
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-05-19 11:00:58 +0200

    replaced mibs with oids as these have to be defined in the OS, and they're not in the travis ubuntu machine where the specs are running

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe NETSNMP::Client do
   subject { described_class.new(host, options) }
 
   describe "#get" do
-    let(:oid) { "sysName.0" }
+    let(:oid) { "1.3.6.1.2.1.1.5.0" } # sysName.0
     let(:value) { subject.get(oid) }
     let(:options) { host_options.merge(context: "a172334d7d97871b72241397f713fa12") }
     it "fetches the varbinds for a given oid" do
@@ -22,7 +22,7 @@ RSpec.describe NETSNMP::Client do
   end
 
   describe "#get_next" do
-    let(:oid) { "sysName.0" }
+    let(:oid) { "1.3.6.1.2.1.1.5.0" } # sysName.0
     let(:value) { subject.get_next(oid) }
     let(:options) { host_options.merge(context: "a172334d7d97871b72241397f713fa12") }
     it "fetches the varbinds for the next oid" do
@@ -31,37 +31,35 @@ RSpec.describe NETSNMP::Client do
   end
 
   describe "#walk" do
-    let(:oid) { "sysORDescr" }
-    let(:oid_code) { "1.3.6.1.2.1.1.9.1.3" }
+    let(:oid) { "1.3.6.1.2.1.1.9.1.3" } # sysORDescr
     let(:value) { subject.walk(oid) }
     let(:options) { host_options.merge(context: "a172334d7d97871b72241397f713fa12") }
     it "fetches the varbinds for the next oid" do
-      expect(value.next).to eq(["#{oid_code}.1","The SNMP Management Architecture MIB."])
-      expect(value.next).to eq(["#{oid_code}.2","The MIB for Message Processing and Dispatching."])
-      expect(value.next).to eq(["#{oid_code}.3","The management information definitions for the SNMP User-based Security Model."])
-      expect(value.next).to eq(["#{oid_code}.4","The MIB module for SNMPv2 entities"])
-      expect(value.next).to eq(["#{oid_code}.5","The MIB module for managing TCP implementations"])
-      expect(value.next).to eq(["#{oid_code}.6","The MIB module for managing IP and ICMP implementations"])
-      expect(value.next).to eq(["#{oid_code}.7","The MIB module for managing UDP implementations"])
-      expect(value.next).to eq(["#{oid_code}.8","View-based Access Control Model for SNMP."])
+      expect(value.next).to eq(["#{oid}.1","The SNMP Management Architecture MIB."])
+      expect(value.next).to eq(["#{oid}.2","The MIB for Message Processing and Dispatching."])
+      expect(value.next).to eq(["#{oid}.3","The management information definitions for the SNMP User-based Security Model."])
+      expect(value.next).to eq(["#{oid}.4","The MIB module for SNMPv2 entities"])
+      expect(value.next).to eq(["#{oid}.5","The MIB module for managing TCP implementations"])
+      expect(value.next).to eq(["#{oid}.6","The MIB module for managing IP and ICMP implementations"])
+      expect(value.next).to eq(["#{oid}.7","The MIB module for managing UDP implementations"])
+      expect(value.next).to eq(["#{oid}.8","View-based Access Control Model for SNMP."])
       expect{ value.next }.to raise_error(StopIteration)
     end
   end
 
   describe "#get_bulk" do
-    let(:oid) { "sysORDescr" }
-    let(:oid_code) { "1.3.6.1.2.1.1.9.1.3" }
+    let(:oid) { "1.3.6.1.2.1.1.9.1.3" }
     let(:value) { subject.get_bulk(oid) }
     let(:options) { host_options.merge(context: "a172334d7d97871b72241397f713fa12") }
     it "fetches the varbinds for the next oid" do
-      expect(value.next).to eq(["#{oid_code}.1","The SNMP Management Architecture MIB."])
-      expect(value.next).to eq(["#{oid_code}.2","The MIB for Message Processing and Dispatching."])
-      expect(value.next).to eq(["#{oid_code}.3","The management information definitions for the SNMP User-based Security Model."])
-      expect(value.next).to eq(["#{oid_code}.4","The MIB module for SNMPv2 entities"])
-      expect(value.next).to eq(["#{oid_code}.5","The MIB module for managing TCP implementations"])
-      expect(value.next).to eq(["#{oid_code}.6","The MIB module for managing IP and ICMP implementations"])
-      expect(value.next).to eq(["#{oid_code}.7","The MIB module for managing UDP implementations"])
-      expect(value.next).to eq(["#{oid_code}.8","View-based Access Control Model for SNMP."])
+      expect(value.next).to eq(["#{oid}.1","The SNMP Management Architecture MIB."])
+      expect(value.next).to eq(["#{oid}.2","The MIB for Message Processing and Dispatching."])
+      expect(value.next).to eq(["#{oid}.3","The management information definitions for the SNMP User-based Security Model."])
+      expect(value.next).to eq(["#{oid}.4","The MIB module for SNMPv2 entities"])
+      expect(value.next).to eq(["#{oid}.5","The MIB module for managing TCP implementations"])
+      expect(value.next).to eq(["#{oid}.6","The MIB module for managing IP and ICMP implementations"])
+      expect(value.next).to eq(["#{oid}.7","The MIB module for managing UDP implementations"])
+      expect(value.next).to eq(["#{oid}.8","View-based Access Control Model for SNMP."])
       expect(value.next).to eq(["1.3.6.1.2.1.1.9.1.4.1",2])
       expect(value.next).to eq(["1.3.6.1.2.1.1.9.1.4.2",2])
       expect{ value.next }.to raise_error(StopIteration)
@@ -74,7 +72,7 @@ RSpec.describe NETSNMP::Client do
 
   describe "#set" do
     let(:options) { host_options.merge(context: "0886e1397d572377c17c15036a1e6c66") } # write cache
-    let(:oid) { "sysUpTimeInstance" }
+    let(:oid) { "1.3.6.1.2.1.1.3.0" } # sysUpTimeInstance
     let(:new_value) { 43 }
     after { subject.set(oid, value: 42) }
     it "updates the value of the oid" do
@@ -88,9 +86,5 @@ RSpec.describe NETSNMP::Client do
       expect(subject.get(oid)).to be(44)
 
     end
-
   end
-
-
-
 end
