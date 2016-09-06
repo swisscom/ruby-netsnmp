7a1c214afeb7dc3e2f9dcbc8f5c203850591c581
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-09-06 12:34:09 +0200

    sleeping to delay test executing, wait for simulator (travis is complaining)

--- spec/support/start_docker.sh
+++ spec/support/start_docker.sh
@@ -1,4 +1,5 @@
 sudo docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t snmp-server-emulator -f spec/support/Dockerfile .
 sudo docker run -d -p :1161/udp --name test-snmp-emulator snmp-server-emulator --agent-udpv4-endpoint=0.0.0.0:1161 --agent-udpv6-endpoint='[::0]:1161'
+sleep 20 # give some time for the simulator to boot
 
 
