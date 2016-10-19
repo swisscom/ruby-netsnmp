397c23ce7d40e808585bb51deb2436dab79cdacc
HoneyryderChuck <cardoso_tiago@hotmail.com>
2016-10-19 16:26:35 +0200

    adding ber translation layer, with integers already covered

--- lib/netsnmp.rb
+++ lib/netsnmp.rb
@@ -8,9 +8,10 @@ module NETSNMP
   def self.version ; Core.version ; end
 end
 
-require 'netsnmp/errors'
-require 'netsnmp/varbind'
-require 'netsnmp/oid'
-require 'netsnmp/pdu'
-require 'netsnmp/session'
-require 'netsnmp/client'
+require "netsnmp/ber"
+require "netsnmp/errors"
+require "netsnmp/varbind"
+require "netsnmp/oid"
+require "netsnmp/pdu"
+require "netsnmp/session"
+require "netsnmp/client"
