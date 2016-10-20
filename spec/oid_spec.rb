597c8caf7fb5122c892b3b0c0c5fc30ac4cf6f6f
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-10-20 20:09:52 +0200

    converting successfully OIDs to ber

--- spec/oid_spec.rb
+++ spec/oid_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe NETSNMP::OID do
   it "translates always to numerical oid code" do
     expect(subject.to_s).to eq(oid_code)
   end
+
+  describe "#to_ber" do
+    it { expect(subject.to_ber).to eq("\x06\b+\x06\x01\x02\x01\x01\x01\x00".b) }
+  end
 end
