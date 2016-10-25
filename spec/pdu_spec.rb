92f8b833618d1f8ffe2a5e9af7040eccf5df3228
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-10-25 19:12:52 +0200

    to_ber -> to_der

--- spec/pdu_spec.rb
+++ spec/pdu_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe NETSNMP::PDU do
   let(:encoded_get_pdu) { "0'\002\001\000\004\006public\240\032\002\002?*\002\001\000\002\001\0000\0160\f\006\b+\006\001\002\001\001\001\000\005\000" }
   let(:encoded_response_pdu) { "0+\002\001\000\004\006public\242\036\002\002'\017\002\001\000\002\001\0000\0220\020\006\b+\006\001\002\001\001\001\000\004\004test" }
 
-  describe "#to_ber" do
+  describe "#to_der" do
     let(:pdu_get){ described_class.build(:get, 
                                          version: 0,
                                          request_id: 16170,
@@ -11,7 +11,7 @@ RSpec.describe NETSNMP::PDU do
 
     context "v1" do
       before { pdu_get.add_varbind(get_request_oid) }
-      it { expect(pdu_get.to_ber).to eq(encoded_get_pdu.b) }
+      it { expect(pdu_get.to_der).to eq(encoded_get_pdu.b) }
     end
   end
 
