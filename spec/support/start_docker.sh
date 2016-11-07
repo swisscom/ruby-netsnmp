d148d99a1d0edb77185c27023f386f92022ccd40
HoneyryderChuck <cardoso_tiago@hotmail.com>
2016-11-07 14:17:31 +0100

    adding users to snmp simulator with different authentication strategies

--- spec/support/start_docker.sh
+++ spec/support/start_docker.sh
@@ -1,5 +1,9 @@
 sudo docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t snmp-server-emulator -f spec/support/Dockerfile .
-sudo docker run -d -p :1161/udp --name test-snmp-emulator snmp-server-emulator --agent-udpv4-endpoint=0.0.0.0:1161 --agent-udpv6-endpoint='[::0]:1161'
+sudo docker run -d -p :1161/udp --name test-snmp-emulator snmp-server-emulator \
+  --agent-udpv4-endpoint=0.0.0.0:1161 --agent-udpv6-endpoint='[::0]:1161' \
+  --v3-user=simulator --v3-auth-key=auctoritas --v3-priv-key=privatus \
+  --v3-user=author --v3-auth-key=authpass --v3-priv-proto=NONE \
+  --v3-user=unsafe --v3-auth-proto=NONE --v3-priv-proto=NONE
 sleep 20 # give some time for the simulator to boot
 
 
