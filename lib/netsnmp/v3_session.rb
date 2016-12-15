12866451afd32f13704d4531551aa0d117f003a5
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-12-15 15:17:09 +0100

    simplified the processes, in that the Message is now an encoder/decoder module, less objects overall, win

--- lib/netsnmp/v3_session.rb
+++ lib/netsnmp/v3_session.rb
@@ -5,7 +5,12 @@ module NETSNMP
       engine_id = @options.fetch(:engine_id, "")
       context   = @options.fetch(:context)
       pdu = ScopedPDU.build(type, headers: [engine_id, context], varbinds: oids)
-      build_message(pdu)
+      build_scoped_pdu(pdu)
+    end
+
+    def send(*)
+      pdu, _ = super
+      pdu
     end
 
     private
@@ -22,26 +27,20 @@ module NETSNMP
       options
     end
 
-    def build_message(pdu)
+    def build_scoped_pdu(pdu)
       if !@security_parameters
-        probe_message = probe_for_engine(pdu)
-        @options[:engine_id] = probe_message.engine_id
+        engine_id = probe_for_engine(pdu)
+        @options[:engine_id] = engine_id
         @security_parameters = SecurityParameters.new(security_level: @options[:security_level], 
                                                       username: @options[:username],
-                                                      engine_id: probe_message.engine_id,
+                                                      engine_id: engine_id,
                                                       auth_protocol: @options[:auth_protocol],
                                                       priv_protocol: @options[:priv_protocol],
                                                       auth_password: @options[:auth_password],
                                                       priv_password: @options[:priv_password])
-        message = Message.new(pdu, security_parameters: @security_parameters,
-                                   engine_id: probe_message.engine_id,
-                                   engine_boots: probe_message.engine_boots,
-                                   engine_time: probe_message.engine_time)
-        message.from_message(probe_message)
-      else
-        message = Message.new(pdu, security_parameters: @security_parameters)
       end
-      message
+      pdu.set_engine_id(@security_parameters.engine_id)
+      pdu
     end
 
     # sends a probe snmp v3 request, to get the additional info with which to handle the security aspect
@@ -49,12 +48,21 @@ module NETSNMP
     def probe_for_engine(pdu)
       report_sec_params = SecurityParameters.new(security_level: 0,
                                                  username: @options[:username])
-      message = Message.new(pdu, security_parameters: report_sec_params)
-      send(message)
+      encoded_report_pdu = Message.encode(pdu, security_parameters: report_sec_params)
+      write(encoded_report_pdu)
+      encoded_response_pdu = read
+      pdu, engine_id, @engine_boots, @engine_time = decode(encoded_response_pdu, security_parameters: report_sec_params)
+      engine_id
     end
 
-    def decode(stream, request)
-      Message.decode(stream, security_parameters: request.security_parameters)
+    def encode(pdu)
+      Message.encode(pdu, security_parameters: @security_parameters, 
+                          engine_boots: @engine_boots,
+                          engine_time: @engine_time)
+    end
+
+    def decode(stream, security_parameters: @security_parameters)
+      Message.decode(stream, security_parameters: security_parameters)
     end
   end
 end
