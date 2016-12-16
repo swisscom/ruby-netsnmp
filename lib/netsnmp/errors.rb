aa76439e89afe5252bbb1d7969ed4037726fea9d
HoneyryderChuck <cardoso_tiago@hotmail.com>
2016-12-16 12:38:03 +0100

    redundant and unused errors

--- lib/netsnmp/errors.rb
+++ lib/netsnmp/errors.rb
@@ -2,7 +2,4 @@ module NETSNMP
   Error = Class.new(StandardError)
   ConnectionFailed = Class.new(Error)
   AuthenticationFailed = Class.new(Error)
-
-  SendError = Class.new(Error)
-  ReceiveError = Class.new(Error)
 end
