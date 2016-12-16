8d752fadd3fe84258c906a4a52349efb14a35091
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-12-16 12:38:39 +0100

    reordered requirements

--- lib/netsnmp.rb
+++ lib/netsnmp.rb
@@ -27,14 +27,13 @@ require "netsnmp/logger"
 require 'netsnmp/core'
 
 module NETSNMP
-  # @return [String] the version of the netsnmp C library
-  def self.version ; Core.version ; end
 end
 
 require "netsnmp/ber"
 require "netsnmp/errors"
-require "netsnmp/varbind"
+
 require "netsnmp/oid"
+require "netsnmp/varbind"
 require "netsnmp/pdu"
 require "netsnmp/session"
 
