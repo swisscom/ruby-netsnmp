090bfde9495e6541df84589088d18defd75b2191
HoneyryderChuck <cardoso_tiago@hotmail.com>
2016-12-16 16:01:55 +0100

    changed the internals of v3 session, so that the security parameters triggers the report pdu

--- lib/netsnmp/v3_session.rb
+++ lib/netsnmp/v3_session.rb
@@ -2,10 +2,9 @@ module NETSNMP
   class V3Session < Session
 
     def build_pdu(type, *oids)
-      engine_id = @options.fetch(:engine_id, "")
+      engine_id = security_parameters.engine_id#@options.fetch(:engine_id, security_parameters.engine_id)
       context   = @options.fetch(:context, "")
       pdu = ScopedPDU.build(type, headers: [engine_id, context], varbinds: oids)
-      build_scoped_pdu(pdu)
     end
 
     def send(*)
@@ -27,27 +26,25 @@ module NETSNMP
       options
     end
 
-    def build_scoped_pdu(pdu)
-      if !@security_parameters
-        engine_id = probe_for_engine(pdu)
-        @options[:engine_id] = engine_id
-        @security_parameters = SecurityParameters.new(security_level: @options[:security_level], 
-                                                      username: @options[:username],
-                                                      engine_id: engine_id,
-                                                      auth_protocol: @options[:auth_protocol],
-                                                      priv_protocol: @options[:priv_protocol],
-                                                      auth_password: @options[:auth_password],
-                                                      priv_password: @options[:priv_password])
-      end
-      pdu.set_engine_id(@security_parameters.engine_id)
-      pdu
+    def security_parameters
+      @security_parameters ||= begin
+        engine_id = probe_for_engine
+        SecurityParameters.new(security_level: @options[:security_level], 
+                               username: @options[:username],
+                               engine_id: engine_id,
+                               auth_protocol: @options[:auth_protocol],
+                               priv_protocol: @options[:priv_protocol],
+                               auth_password: @options[:auth_password],
+                               priv_password: @options[:priv_password])
+      end 
     end
 
     # sends a probe snmp v3 request, to get the additional info with which to handle the security aspect
     #
-    def probe_for_engine(pdu)
+    def probe_for_engine
       report_sec_params = SecurityParameters.new(security_level: 0,
                                                  username: @options[:username])
+      pdu = ScopedPDU.build(:get, headers: [])
       encoded_report_pdu = Message.encode(pdu, security_parameters: report_sec_params)
 
       encoded_response_pdu = @transport.send(encoded_report_pdu)
