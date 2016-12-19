22f958b4e2ee5571a7dcf1b64c2c404edf717c69
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-12-19 14:07:15 +0100

    added the custom engine id to the simulator

--- spec/support/start_docker.sh
+++ spec/support/start_docker.sh
@@ -1,5 +1,6 @@
 sudo docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t snmp-server-emulator -f spec/support/Dockerfile .
 sudo docker run -d -p :1161/udp --name test-snmp-emulator snmp-server-emulator \
+  --v3-engine-id=000000000000000000000002 \
   --agent-udpv4-endpoint=0.0.0.0:1161 --agent-udpv6-endpoint='[::0]:1161' \
   --v3-user=simulator --v3-auth-key=auctoritas --v3-priv-key=privatus \
   --v3-user=authmd5 --v3-auth-key=maplesyrup --v3-auth-proto=MD5 --v3-priv-proto=NONE \
