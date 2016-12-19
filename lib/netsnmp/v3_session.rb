8097d28eeb7b9e1e05d98963b4eb9924c771ebd2
Tiago Cardoso <tiago.cardoso@swisscom.com>
2016-12-19 14:05:51 +0100

    allowing to pass a custom security parameters object to the v3 session; the benefit is that the performance bottlenecks can be rearranged on the outside, and this also allows to pass the same object with the auth_ and priv_keys eager-loaded (the biggest performance bottleneck, specially, when we use the same in multiple clients)

--- lib/netsnmp/v3_session.rb
+++ lib/netsnmp/v3_session.rb
@@ -2,6 +2,11 @@
 module NETSNMP
   class V3Session < Session
 
+    def initialize(*)
+      super
+      @security_parameters = @options.delete(:security_parameters)
+    end
+
     def build_pdu(type, *oids)
       engine_id = security_parameters.engine_id
       context   = @options.fetch(:context, "")
@@ -17,34 +22,36 @@ module NETSNMP
 
     def validate_options(options)
       options = super
-      options[:security_level] = case options[:security_level]
-        when /no_?auth/         then 0
-        when /auth_?no_?priv/   then 1
-        when /auth_?priv/, nil  then 3
-        when Integer
-          options[:security_level]
+      if s = @security_parameters
+        # inspect public API
+        unless s.respond_to?(:encode) &&
+               s.respond_to?(:decode) &&
+               s.respond_to?(:sign)   &&
+               s.respond_to?(:verify)
+          raise Error, "#{s} doesn't respect the sec params public API (#encode, #decode, #sign)" 
+        end 
       end
       options
     end
 
     def security_parameters
-      @security_parameters ||= begin
-        engine_id = probe_for_engine
-        SecurityParameters.new(security_level: @options[:security_level], 
-                               username: @options[:username],
-                               engine_id: engine_id,
-                               auth_protocol: @options[:auth_protocol],
-                               priv_protocol: @options[:priv_protocol],
-                               auth_password: @options[:auth_password],
-                               priv_password: @options[:priv_password])
-      end 
+      @security_parameters ||= SecurityParameters.new(security_level: @options[:security_level], 
+                                                      username: @options[:username],
+                                                      auth_protocol: @options[:auth_protocol],
+                                                      priv_protocol: @options[:priv_protocol],
+                                                      auth_password: @options[:auth_password],
+                                                      priv_password: @options[:priv_password])
+      if @security_parameters.engine_id.empty?
+        @security_parameters.engine_id = probe_for_engine
+      end
+      @security_parameters
     end
 
     # sends a probe snmp v3 request, to get the additional info with which to handle the security aspect
     #
     def probe_for_engine
       report_sec_params = SecurityParameters.new(security_level: 0,
-                                                 username: @options[:username])
+                                                 username: @security_parameters.username)
       pdu = ScopedPDU.build(:get, headers: [])
       encoded_report_pdu = Message.encode(pdu, security_parameters: report_sec_params)
 
