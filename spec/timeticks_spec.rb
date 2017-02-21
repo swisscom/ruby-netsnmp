26dd33596a1e113c2042e1aa92fc77ac8a345607
HoneyryderChuck <cardoso_tiago@hotmail.com>
2017-02-21 12:44:28 +0100

    introducing timetick special type, which coerces to numeric, handles (most) numeric operations, and stringifies to an expected representation, at least comparing to what the C netsnmp library does

---
+++ spec/timeticks_spec.rb
@@ -0,0 +1,12 @@
+# from https://ask.wireshark.org/questions/14002/how-to-decode-timeticks-hundreds-seconds-to-readable-date-time
+RSpec.describe NETSNMP::Timetick do
+  subject { described_class.new(1525917187) }
+
+  describe "as an integer" do
+    it { expect((1 + subject).to_i).to be(1525917188) }
+  end
+
+  describe "as an embedded string" do
+    it { expect(subject.to_s).to eq("Timeticks: (1525917187) 176 days, 14:39:31.87") }
+  end
+end
