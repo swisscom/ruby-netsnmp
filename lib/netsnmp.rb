31a179aacabd14d426d695921b95352c5040bc25
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-02-08 00:17:18 +0000

    added backports for ruby 2.1

--- lib/netsnmp.rb
+++ lib/netsnmp.rb
@@ -71,3 +71,25 @@ require "netsnmp/encryption/des"
 require "netsnmp/encryption/aes"
 
 require "netsnmp/client"
+
+unless Numeric.method_defined?(:positive?)
+  # Ruby 2.3 Backport (Numeric#positive?)
+  #
+  module PosMethods
+    def positive?
+      self > 0
+    end
+  end
+  Numeric.__send__(:include, PosMethods)
+end
+
+unless String.method_defined?(:+@)
+  # Backport for +"", to initialize unfrozen strings from the string literal.
+  #
+  module LiteralStringExtensions
+    def +@
+      frozen? ? dup : self
+    end
+  end
+  String.__send__(:include, LiteralStringExtensions)
+end
